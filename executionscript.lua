e58abf2d4c16571e37080b0d78665d95a8160e30
amin <amin@contact.im>
2024-12-23 20:11:23 -0500

    Update and rename loadstring.lua to executionscript.lua

---
+++ executionscript.lua
@@ -0,0 +1,2 @@
+-- THE OFFICIAL PRISONSPLOIT EXECUTION SCRIPT. THIS IS USED TO LOAD THE MAIN SCRIPT. --
+loadstring(game:HttpGet("https://raw.githubusercontent.com/itsaminowo/PrisonSploit/main/maincode.lua"))()
--- loadstring.lua
+++
@@ -1,2 +0,0 @@
--- THE OFFICIAL PRISONSPLOIT LOADSTRING. THIS IS USED TO LOAD THE SCRIPT. --
-loadstring(game:HttpGet("https://raw.githubusercontent.com/itsaminowo/PrisonSploit/main/maincode.lua"))()
